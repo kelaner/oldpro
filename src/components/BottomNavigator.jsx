14693b37712509a94f3f943bdce588201b2ba5aa
laner <lanercifang@outlook.com>
2023-05-13 00:03:53 +0800

    test

--- src/components/BottomNavigator.jsx
+++ src/components/BottomNavigator.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons'
